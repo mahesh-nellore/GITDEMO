299f9d197eb8ccc6bc1c52313f23c26f51b5bd23
unknown <nellore.mahesh866@gmail.com>
2021-12-01 21:15:45 +0530

    Added the main class

--- src/demo/Test1.java
+++ src/demo/Test1.java
@@ -5,5 +5,12 @@ public class Test1 {
 	static int add(int a, int b) {
 		return a+b;
 	}
+	static int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
